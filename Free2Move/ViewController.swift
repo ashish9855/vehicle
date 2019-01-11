a03f66427576a628f31f27217d85c84b01189c28
Ashish Maheshwari <ashish.maheshwari@tajawal.com>
2019-01-11 14:30:29 +0100

    Initial commit

---
+++ Free2Move/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Free2Move
+//
+//  Created by Ashish Maheshwari on 1/11/19.
+//  Copyright © 2019 Ashish Maheshwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
