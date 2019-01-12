19d043285b0545ba0e2b2f249d6761795770da0b
Ashish Maheshwari <ashish.maheshwari@tajawal.com>
2019-01-12 15:11:40 +0100

    Add vehicle position model class

---
+++ Free2Move/Home/Entities/VehiclePositionModel.swift
@@ -0,0 +1,10 @@
+//
+//  VehiclePositionModel.swift
+//  Free2Move
+//
+//  Created by Ashish Maheshwari on 1/12/19.
+//  Copyright © 2019 Ashish Maheshwari. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
