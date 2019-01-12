b8b66df6d31c00bd449c6844274db90c8b658bd3
Ashish Maheshwari <ashish.maheshwari@tajawal.com>
2019-01-12 16:36:58 +0100

    Parse vehicle model

--- Free2Move/Home/Entities/VehicleModel.swift
+++ Free2Move/Home/Entities/VehicleModel.swift
@@ -8,3 +8,29 @@
 
 import Foundation
 import SwiftyJSON
+
+struct Vehicle {
+    let provider: String
+    let manufacturer: String
+    let model: String
+    let sign: String
+    let vin: String
+    let fuelState: Int
+    let engineType: String
+    let automatic: Bool
+    let color: String
+    let position: Position
+    
+    init(_ json: JSON) {
+        provider = json["provider"].stringValue
+        manufacturer = json["manufacturer"].stringValue
+        model = json["model"].stringValue
+        sign = json["sign"].stringValue
+        vin = json["vin"].stringValue
+        fuelState = json["fuel_state"].intValue
+        engineType = json["engine_type"].stringValue
+        automatic = json["automatic"].boolValue
+        color = json["color"].stringValue
+        position = Position(json["position"])
+    }
+}
