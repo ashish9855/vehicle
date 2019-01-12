5316c9a473d7c800616da3c0476f7b60c31d02ed
Ashish Maheshwari <ashish.maheshwari@tajawal.com>
2019-01-12 19:48:26 +0100

    Read content of file

--- Free2MoveTests/Utils/File.swift
+++ Free2MoveTests/Utils/File.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+@testable import Free2Move
+
+typealias JSONDictionary = [String: Any]
+
+class TestContentProvider {
+   
+    static func contentsOfFile(_ fileName: String) -> JSONDictionary{
+        let components = fileName.components(separatedBy: ".")
+        let name = components.first!
+        let fileExtension = components.last!
+        let path = Bundle.main.path(forResource: name, ofType: fileExtension)
+        let data = try! NSData(contentsOfFile: path!, options:.mappedIfSafe) as Data
+        let result = try! JSONSerialization.jsonObject(with: data as Data, options: .mutableContainers)
+        return result as! JSONDictionary
+    }
+}
