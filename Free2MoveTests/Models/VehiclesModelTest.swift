1f1d718f7227b2698b48aef7b9b211fcd57f3dcb
Ashish Maheshwari <ashish.maheshwari@tajawal.com>
2019-01-13 08:31:36 +0100

    Write test case for vehicles model

--- Free2MoveTests/Models/VehiclesModelTest.swift
+++ Free2MoveTests/Models/VehiclesModelTest.swift
@@ -18,25 +18,44 @@ class VehiclesModelTest: XCTestCase {
         super.setUp()
         vehicles = Vehicles(JSON(fileName: "Vehicles.json"))
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
     
     func testItemsCount_WhenDataParsed() {
         XCTAssertEqual(vehicles?.vehicles.count, 2)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testVehicleData_WhenDataParsed() {
+        let vehicle = vehicles?.vehicles[0]
+        XCTAssertEqual(vehicle?.provider, "vehicle4go")
+        XCTAssertEqual(vehicle?.manufacturer, "Mercedes-Benz")
+        XCTAssertEqual(vehicle?.model, "Mercedes-Benz B-Klasse")
+        XCTAssertEqual(vehicle?.sign, "i5q9KV")
+        XCTAssertEqual(vehicle?.vin, "cRgbshhbfSwc")
+        XCTAssertEqual(vehicle?.fuelState, 72)
+        XCTAssertEqual(vehicle?.engineType, Vehicle.EngineType.electric)
+        XCTAssertEqual(vehicle?.automatic, true)
+        XCTAssertEqual(vehicle?.color, "laurus_grey")
+        XCTAssertEqual(vehicle?.position.latitutde, 52.30758365322001)
+        XCTAssertEqual(vehicle?.position.longitude, 13.360824981446907)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testVehicleData_WhenDataIsNULL_ThenDefaultData() {
+        let vehicle = vehicles?.vehicles[1]
+        XCTAssertEqual(vehicle?.provider, "")
+        XCTAssertEqual(vehicle?.manufacturer, "")
+        XCTAssertEqual(vehicle?.model, "")
+        XCTAssertEqual(vehicle?.sign, "")
+        XCTAssertEqual(vehicle?.vin, "")
+        XCTAssertEqual(vehicle?.fuelState, 0)
+        XCTAssertEqual(vehicle?.engineType, Vehicle.EngineType.generic)
+        XCTAssertEqual(vehicle?.automatic, false)
+        XCTAssertEqual(vehicle?.color, "")
+        XCTAssertEqual(vehicle?.position.latitutde, 0)
+        XCTAssertEqual(vehicle?.position.longitude, 0)
     }
-
+    
+    
+    func testEngineType_WhenNotOfDefinedType_ThenGenericType() {
+        let vehicle = vehicles?.vehicles[1]
+        XCTAssertEqual(vehicle?.engineType, Vehicle.EngineType.generic)
+    }
 }
