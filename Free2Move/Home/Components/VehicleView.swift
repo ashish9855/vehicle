a477a574cb9653d9f3728a89aa852fc91fc6e916
Ashish Maheshwari <ashish.maheshwari@tajawal.com>
2019-01-15 21:22:36 +0100

    Create vehicle view

---
+++ Free2Move/Home/Components/VehicleView.swift
@@ -0,0 +1,21 @@
+//
+//  VehicleView.swift
+//  Free2Move
+//
+//  Created by Ashish Maheshwari on 1/15/19.
+//  Copyright © 2019 Ashish Maheshwari. All rights reserved.
+//
+
+import UIKit
+
+class VehicleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
