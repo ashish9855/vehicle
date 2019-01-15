90993a28ba6f07ead2932da7e487210423b45b72
Ashish Maheshwari <ashish.maheshwari@tajawal.com>
2019-01-15 19:39:30 +0100

    Add anotations and show them on the screen

--- Free2Move/Home/Controllers/HomeViewController.swift
+++ Free2Move/Home/Controllers/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class HomeViewController: UIViewController, RootViewProtocol {
 
@@ -16,14 +17,37 @@ class HomeViewController: UIViewController, RootViewProtocol {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
-        businessLayer.getVehicles(success: { (vehicles) in
-            print(vehicles)
+        self.rootView.startLoading()
+        businessLayer.getVehicles(success: {[weak self] (vehicles) in
+            self?.rootView.stopLoading()
+            self?.rootView.vehicles = vehicles.vehicles
+            self?.rootView.convertCoordinatesToAnnotations(for: vehicles.vehicles)
+            self?.rootView.zoomMap()
+            self?.rootView.addAllAnotations()
         }, failure: { (error) in
             print(error)
+            self.rootView.stopLoading()
+
         })
+    }
+}
+
+extension HomeViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var pinView: MKPinAnnotationView?
+        let defaultPinID = "myAnnotation"
+        
+        pinView = mapView.dequeueReusableAnnotationView(withIdentifier: defaultPinID) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: defaultPinID)
+            pinView!.canShowCallout = true
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
     }
 }
 
--- Free2Move/Home/Controllers/HomeViewControllerRootView.swift
+++ Free2Move/Home/Controllers/HomeViewControllerRootView.swift
@@ -7,8 +7,62 @@
 //
 
 import UIKit
+import MBProgressHUD
+import MapKit
 
 class HomeViewControllerRootView: UIView {
     
+    @IBOutlet weak var mapView: MKMapView!
     
+    var vehicles: [Vehicle] = []
+    var annotations:[MKPointAnnotation] = []
+    
+    func convertCoordinatesToAnnotations(for vehicles: [Vehicle]) {
+        for vehicle in vehicles {
+            let annotation = getAnnotation(vehicle: vehicle)
+            annotations.append(annotation)
+        }
+    }
+    
+    func removeAllAnotations() {
+        mapView.removeAnnotations(mapView.annotations)
+    }
+    
+    func addAllAnotations() {
+        if !annotations.isEmpty {
+            removeAllAnotations()
+            mapView.addAnnotations(annotations)
+        }
+    }
+    
+    private func getAnnotation(vehicle: Vehicle) -> MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        let latitude = vehicle.position.latitutde
+        let longitude = vehicle.position.longitude
+        let coordinates = CLLocationCoordinate2D.init(latitude: latitude, longitude: longitude)
+        annotation.coordinate = coordinates
+        return annotation
+    }
+    
+    func startLoading() {
+        MBProgressHUD.showAdded(to: self, animated: true)
+    }
+    
+    func stopLoading() {
+        MBProgressHUD.hide(for: self, animated: true)
+    }
+    
+    func zoomMap() {
+        guard let firstCoordinate = annotations.first?.coordinate else {
+            return }
+        let annotationPoint = MKMapPoint(firstCoordinate)
+        var zoomRect =  MKMapRect(x: annotationPoint.x, y: annotationPoint.y, width: 0, height: 0)
+        if let lastCoordinate = annotations.last?.coordinate {
+            let lastAnnotationPoint = MKMapPoint(lastCoordinate)
+            let pointLastRect = MKMapRect(x: lastAnnotationPoint.x, y: lastAnnotationPoint.y, width: 0, height: 0)
+            zoomRect = zoomRect.union(pointLastRect)
+        }
+        
+        mapView.setVisibleMapRect(zoomRect, animated: true)
+    }
 }
