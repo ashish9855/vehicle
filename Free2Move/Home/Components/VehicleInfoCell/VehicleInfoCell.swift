9dc3c28fdc56a40589dda5886a3cee005db2280d
Ashish Maheshwari <ashish.maheshwari@tajawal.com>
2019-01-15 21:36:32 +0100

    Create vehicle info cell

---
+++ Free2Move/Home/Components/VehicleInfoCell/VehicleInfoCell.swift
@@ -0,0 +1,18 @@
+//
+//  VehicleInfoCell.swift
+//  Free2Move
+//
+//  Created by Ashish Maheshwari on 1/15/19.
+//  Copyright © 2019 Ashish Maheshwari. All rights reserved.
+//
+
+import UIKit
+
+class VehicleInfoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
